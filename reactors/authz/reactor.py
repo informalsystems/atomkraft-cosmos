80197cde1dc1e87fe007c49b2b5334e04e53d95c
hvanz <hernan.vanzetto@gmail.com>
2022-09-25 23:14:53 +0200

    refactor reactor for UnicodeDecodeError

--- reactors/authz/reactor.py
+++ reactors/authz/reactor.py
@@ -87,18 +87,11 @@ def request_revoke(
     assert event.type == "request-revoke"
     request = model.MsgRevoke(unmunchify(event))
 
-    try:
-        result = testnet.broadcast_transaction(
+    result = catch_unicode_decode_error(
+        lambda: testnet.broadcast_transaction(
             request.granter, request.to_real(testnet), gas=200000, fee_amount=20000
         )
-    except UnicodeDecodeError as e:
-        # Terra library aborts execution with UnicodeDecodeError: 'utf-8' codec can't decode byte 0x83 in position 85: invalid start byte
-        # Quick hack to fail without halting:
-        result = TxResponse(
-            codespace="authz",
-            code=2,
-            raw_log="failed to delete grant with key ...; authorization not found",
-        )
+    )
 
     show_result(result, expectedResponse)
     check_result(result, expectedResponse)
@@ -114,18 +107,11 @@ def request_execute(
     assert event.type == "request-execute"
     request = model.MsgExec(unmunchify(event))
 
-    try:
-        result = testnet.broadcast_transaction(
+    result = catch_unicode_decode_error(
+        lambda: testnet.broadcast_transaction(
             request.grantee, request.to_real(testnet), gas=200000, fee_amount=20000
         )
-    except UnicodeDecodeError as e:
-        # Terra library aborts execution with UnicodeDecodeError: 'utf-8' codec can't decode byte 0x83 in position 85: invalid start byte
-        # Quick hack to fail without halting:
-        result = TxResponse(
-            codespace="authz",
-            code=2,
-            raw_log="failed to execute message; authorization not found",
-        )
+    )
 
     show_result(result, expectedResponse)
     check_result(result, expectedResponse)
@@ -170,3 +156,16 @@ def expire(
     logging.info("Waiting for the grant to expire...")
     logging.info(f"Waiting {model.EXPIRES_SOON_TIME} seconds")
     time.sleep(model.EXPIRES_SOON_TIME + 5)
+
+
+# Quick hack to return fail without halting the trace execution.
+def catch_unicode_decode_error(fn):
+    try:
+        return fn()
+    except UnicodeDecodeError as e:
+        # Terra library aborts execution with UnicodeDecodeError: 'utf-8' codec can't decode byte 0x83 in position 85: invalid start byte
+        return TxResponse(
+            codespace="authz",
+            code=2,
+            raw_log="failed to execute message; UnicodeDecodeError",
+        )
